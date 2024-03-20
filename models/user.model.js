b39f4096f2ee20d93ecb9d96c37451091a9c9b1a
imlnr <imlnr2002@gmail.com>
2024-03-20 12:24:26 +0530

    final commit

---
+++ models/user.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true }
+}, {
+    versionKey: false
+})
+
+const UserModel = mongoose.model("Users", userSchema);
+
+module.exports = {
+    UserModel
+}
