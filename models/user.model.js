4069b6d5807b6d734f61db0b4056fb552aeee2d8
imlnr <imlnr2002@gmail.com>
2024-03-20 12:30:12 +0530

    final commit

--- models/user.model.js
+++ models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin: { type: Boolean, required: true }
+    isAdmin: { type: Boolean, default: false }
 }, {
     versionKey: false
 })
